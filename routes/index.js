ea47fbfb57b51304addc76abd574023a466cd533
Shaktidhar <sdmohapatra7@gmail.com>
2023-03-26 23:16:30 +0530

    added ausers conntoller and a users route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const homeController = require('../controllers/home_controller')
 
 console.log('Routers loaded..')
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+//for any further routes access from here
+//router.use('/routeNmae',require('./routerFile'));
 
 module.exports = router;
