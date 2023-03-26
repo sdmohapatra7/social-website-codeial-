51a188e7c2a882c8b85c690508333171e85a56d7
Shaktidhar <sdmohapatra7@gmail.com>
2023-03-26 22:07:11 +0530

    first commit : creat the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 2000;
+
+
+
+app.listen(port,function(err){
+    if(err){
+        // console.log('Error: ',err);
+        //another way to print error in backtiks using interpolation with in backtiks then doler then curly brackets...
+        console.log(`Error in running the server : ${err}`);
+    }
+    console.log(`Server Is Running On port : ${port}`);
+});
