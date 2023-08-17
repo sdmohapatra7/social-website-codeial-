ed7648ac53b416e4b89e2991162424560c7ea322
Shaktidhar <sdmohapatra7@gmail.com>
2023-08-17 15:42:14 +0530

    complete toogle like controller and route

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.use('/users',require('./users'));
 //router.use('/routeNmae',require('./routerFile'));
 router.use('/post',require('./post'));
 router.use('/comments',require('./comments'));
+router.use('/likes',require('./likes'));
 router.use('/api',require('./api'));
 module.exports = router;
